bd90c53bd5491a8396378e17237d9efc55112f3a
Rafael Almeida <almeidaraf@gmail.com>
2021-04-07 00:05:46 +0100

    Add support to play as black

--- chess.cc
+++ chess.cc
@@ -70,26 +70,43 @@ int main(int argc, char *argv[]) {
 
     };
     Board board;
-    board.NewTurn(kWhite);
+    Color mycolor = kBlack;
+    bool first_move = false;
     while (std::getline(std::cin, line)) {
       std::string command = line.substr(0, line.find(" "));
       if (command == "quit") {
         return 0;
       } else if (command == "protover") {
         std::cout << "feature reuse=0 sigint=0 sigterm=0" << std::endl;
+      } else if (command == "white") {
+        mycolor = kWhite;
+      } else if (command == "black") {
+        mycolor = kBlack;
+      } else if (command == "go") {
+        board.NewTurn(mycolor);
+        auto valid_ai_moves = board.GetMoves(mycolor);
+        Move ai_move = ChooseAiMove(board, mycolor, 4, SmartUtility);
+        board.DoMove(ai_move);
+        std::cout << "move " << ai_move.XboardString() << std::endl;
+        board.NewTurn(Other(mycolor));
       } else if (ignored.find(command) != ignored.end()) {
         // ignore
       } else {
         // not a known command, must be a move
+        if (first_move && Other(mycolor) == kWhite) {
+          board.NewTurn(kWhite);
+        }
+        first_move = false;
         auto human_move = Move::FromXboardString(command);
         if (!human_move.has_value()) {
           std::cout << "Ilegal move: " << command << std::endl;
           continue;
         }
         board.DoMove(*human_move);
-        board.NewTurn(kBlack);
-        auto valid_ai_moves = board.GetMoves(kBlack);
-        switch (GetGameOutcome(!valid_ai_moves.empty(), board.IsCheck(kBlack))) {
+
+        board.NewTurn(mycolor);
+        auto valid_ai_moves = board.GetMoves(mycolor);
+        switch (GetGameOutcome(!valid_ai_moves.empty(), board.IsCheck(mycolor))) {
         case kCheckmate:
           std::cout << "1-0 {White mates}" << std::endl;
           continue;
@@ -98,12 +115,13 @@ int main(int argc, char *argv[]) {
           continue;
         }
 
-        Move ai_move = ChooseAiMove(board, kBlack, 3, SmartUtility);
+        Move ai_move = ChooseAiMove(board, mycolor, 4, SmartUtility);
         board.DoMove(ai_move);
         std::cout << "move " << ai_move.XboardString() << std::endl;
-        board.NewTurn(kWhite);
-        auto valid_human_moves = board.GetMoves(kWhite);
-        switch (GetGameOutcome(!valid_human_moves.empty(), board.IsCheck(kWhite))) {
+
+        board.NewTurn(Other(mycolor));
+        auto valid_human_moves = board.GetMoves(Other(mycolor));
+        switch (GetGameOutcome(!valid_human_moves.empty(), board.IsCheck(Other(mycolor)))) {
         case kCheckmate:
           std::cout << "0-1 {Black mates}" << std::endl;
           continue;
